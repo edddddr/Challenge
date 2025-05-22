bebd5f877d265588334c258181ed443775d0b23e
edddddr <endt6342@gmail.com>
2025-05-21 21:32:05 -0900

    I used math library to get the expected result

--- series1.cpp
+++ series1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
@@ -8,10 +9,10 @@ double enteredNum = 5;
 double sum = 0;
 
     for(int i=1; i<enteredNum; i++){
-        sum += 1 / (i*i);
+        sum += 1 / pow(i, i);
     }
 
-    cout<<"The result is : "<<endl;
+    cout<<"The result is : "<<sum<<endl;
 
     return 0;
 }
