d44078348be8cdb4e1f681318a440324e33b70b6
edddddr <endt6342@gmail.com>
2025-05-27 00:57:29 -0900

    math library have not a cube method, but with pow method we can use

--- cubeUptonum.cpp
+++ cubeUptonum.cpp
@@ -6,7 +6,15 @@ using namespace std;
 
 int main(){
 
- 
+ int n;
+ cout<<"Enter a number : ";
+ cin>>n;
+ int cub=0;
+
+ for(int i =1; i<=n; i++){
+    cub = pow(i, 3);
+    cout<<"Number is : "<<i<<" the cube of "<<i<<" is "<<cub<<endl;
+ }
 
     return 0;
 }
