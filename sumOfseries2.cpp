cef3ae93769c20a69ff2ae794276bbd3c81e1048
edddddr <endt6342@gmail.com>
2025-05-27 00:33:28 -0900

    Is this series

--- sumOfseries2.cpp
+++ sumOfseries2.cpp
@@ -1,1 +1,20 @@
 #include<iostrem>
+
+using namespace std;
+
+
+int main(){
+
+    int n;
+    int sum =0;
+    cout<<"Enter a number to find series "
+    cin>>n;
+
+    for(int i=1; i<=n i++){
+        sum+=i;
+    }
+
+    cout<<sum;
+    return 0;
+
+}
