91ab6622c47dd89b7241fe3d0478bb7c4c4910d4
edddddr <endt6342@gmail.com>
2025-06-04 20:48:13 -0900

    Math is matter

--- reversed.cpp
+++ reversed.cpp
@@ -5,13 +5,18 @@ using namespace std;
 
 int main(){
 
+    // My code 
+    /*
     int num_array[5] = {1,2,3,4,5};
 
     cout<<num_array[0];
 
     int reversed_array[5];
     int counter = 0;
-    //cout<<"Input a number : ";
+    cout<<"Input a number : ";
+    for(int i =0; i <= 5; i++){
+        cin>>num_array[]
+    }
     for(int i = 4; i >= 0; i--){
          reversed_array[counter] = num_array[i];
          counter++;
@@ -22,6 +27,22 @@ int main(){
              counter++;
         }
 
+        */
+
+
+    // Other's code
+    int num, sum = 0, t,r;
+
+    cout<<"Eneter a number : ";
+    cin>>num;
+
+
+    for(t=num; num != 0; num = num / 10){
+        r = num % 10;
+        sum = sum * 10 + r;
+    }
+
+    cout<<"The reversed number is : "<<sum;
 
     return 0;
 }
