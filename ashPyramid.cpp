e509b193976844c6c1da2215aded78c365b81e32
edddddr <endt6342@gmail.com>
2025-06-10 19:37:32 -0900

    1122333

--- ashPyramid.cpp
+++ ashPyramid.cpp
@@ -14,6 +14,15 @@ int main(){
     }
 
 
+    for(int i=1; i <= 5; i++ ){
+        for(int j=1; j <=i; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+
+    
+
     return 0;
 
 }
