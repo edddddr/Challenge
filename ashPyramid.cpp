f1a39ab293f1b76d5d591557fee3796681b7cfa6
edddddr <endt6342@gmail.com>
2025-06-10 19:47:51 -0900

    1121231234

--- ashPyramid.cpp
+++ ashPyramid.cpp
@@ -20,6 +20,13 @@ int main(){
         }
         cout<<endl;
     }
+    
+    for(int i=1; i <= 5; i++ ){
+        for(int j=1; j <=i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
 
     
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
-#include<vector>
 
 
 using namespace std;
 
+
 int main(){
 
-    int x =1234;
-    cout<<x % 10<<endl;
+    for(int i=1; i <= 5; i++ ){
+        for(int j=1; j <=i; j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
 
-    //cin>>x;
+    return 0;
+
 }
