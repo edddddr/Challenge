91e344efc00c1b51b679aa14a25113ee76293704
edddddr <endt6342@gmail.com>
2025-06-11 20:04:35 -0900

    I can't believe

--- ashPyramid.cpp
+++ ashPyramid.cpp
@@ -28,6 +28,25 @@ int main(){
         cout<<endl;
     }
 
+
+    for(int i=1; i <= 5; i++ ){
+        for(int j=5; j > i; j--){
+            cout<<" ";
+        }
+         for(int y =1; y<=i; y++){
+            cout<<y;
+            cout<<" ";
+         }
+        cout<<endl;
+    }
+
+    // The out put of the above code is :  
+    // 1
+    // 1 2
+//    1 2 3
+//   1 2 3 4
+// 1 2 3 4 5
+
     
 
     return 0;
