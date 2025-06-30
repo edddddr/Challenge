4a986d1dedc3952069aba8bdcf2ee8dc19ae55aa
edddddr <endt6342@gmail.com>
2025-06-29 21:05:01 -0900

    functionality

--- string_ex/largestWord.cpp
+++ string_ex/largestWord.cpp
@@ -4,13 +4,34 @@ using namespace std;
 
 string largestWord(string setence){
 
+    int L_word = 0;
+    int count = 0;
+    string word = 'w';
+
+    for(int i=0; i<setence.length(); i++){
+        char_code = char(str[0])
+        while(char_code != " "){
+            count++;
+            word+=char_code;
+        }
+        if(count >  L_word){
+            L_word = count;
+           string word = word; 
+        }
     
+        count = 0;
+        Lword = 'w';
+        }
+        
+
+        return word;
 
 }
 
 
 int main(){
 
+    cout<<"The largerst word in the stentence is : "<<largestWord('C++ is a computer language')<<endl;
 
     return 0;
 }
